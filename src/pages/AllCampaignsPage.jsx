28cb70273d7929243ea1f742e789eced8033970b
sajjadislam523 <sajjad.islam523@gmail.com>
2024-12-08 04:24:07 +0600

    Added ascending discending button in the all capaign page

--- src/pages/AllCampaignsPage.jsx
+++ src/pages/AllCampaignsPage.jsx
@@ -1,9 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import CampaignCard from "./CampaignCard";
 import { Typewriter } from "react-simple-typewriter";
+import { useContext, useState } from "react";
+import { ThemeContext } from "../context/ThemeProvider";
 
 const AllCampaignsPage = () => {
     const campaigns = useLoaderData();
+    const [sortedCampaigns, setSortedCampaigns] = useState(campaigns);
+    const [sortOrder, setSortOrder] = useState("asc");
+    const { theme } = useContext(ThemeContext);
+
+    const handleSort = (order) => {
+        const sorted = [...sortedCampaigns].sort((a, b) => {
+            if (order === "asc") {
+                return a.minimumDonation - b.minimumDonation;
+            } else {
+                return b.minimumDonation - a.minimumDonation;
+            }
+        });
+        setSortOrder(order);
+        setSortedCampaigns(sorted);
+    };
 
     return (
         <div className="px-6 py-8 all-campaigns">
@@ -18,8 +35,36 @@ const AllCampaignsPage = () => {
                     delaySpeed={1500}
                 />
             </h1>
+
+            <div className="mb-4 text-center">
+                <p>
+                    {" "}
+                    <strong>Order:</strong>{" "}
+                    {sortOrder === "asc" ? "Ascending" : "Descending"}
+                </p>
+            </div>
+
+            <div className="mb-4 text-center">
+                <button
+                    className={`px-4 py-2 ml-4 border rounded-full font-nunito border-gray-500 ${
+                        theme === "dark" ? "text-white" : "text-black"
+                    } hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-800 dark:hover:text-white transition duration-300`}
+                    onClick={() => handleSort("asc")}
+                >
+                    Ascending
+                </button>
+                <button
+                    className={`px-4 py-2 ml-4 border rounded-full font-nunito border-gray-500 ${
+                        theme === "dark" ? "text-white" : "text-black"
+                    } hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-800 dark:hover:text-white transition duration-300`}
+                    onClick={() => handleSort("desc")}
+                >
+                    Descending
+                </button>
+            </div>
+
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                {campaigns.map((campaign) => (
+                {sortedCampaigns.map((campaign) => (
                     <CampaignCard key={campaign._id} campaigns={campaign} />
                 ))}
             </div>
